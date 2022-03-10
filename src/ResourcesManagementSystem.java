ab0e44439621b45f5abe55483a0c4b8463f6762d
space <space@wooju.mshome.net>
2022-03-10 22:36:18 +0900

    added ResourcesManagementSystem.java

---
+++ src/ResourcesManagementSystem.java
@@ -0,0 +1,13 @@
+
+public class ResourcesManagementSystem {
+	public static void main(String[] args) {
+		System.out.println("Add Schedule");
+		System.out.println("Delete Schedule");
+		System.out.println("Edit Schedule");
+		System.out.println("Get Money");
+		System.out.println("Spend Money");
+		System.out.println("The Money Left");
+		System.out.println("Go Out");
+	}
+
+}
